5cadb5aefd2da6b3d3067c99fc228aacc5303ece
Oleksii <alexmkh89@gmail.com>
2020-05-08 19:43:37 +0300

    Simplified task works.

---
+++ HomeworkDeleagatesEvents/CarWash.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkDeleagatesEvents
+{
+    public class CarWash
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public static List<CarWash> CarWashList { get;private set; } = new List<CarWash>();
+
+        public delegate void Notification(CarWash carWash);
+        public event Notification CarStatusNotification;
+
+
+        public CarWash(string name, int price)
+        {
+            Name = name;
+            Price = price;
+            CarWashList.Add(this);
+        }
+
+
+        public void CarProcessing(Car car)
+        {
+            // Getting car cleaning if its balance is enaugh.
+            if (car.Balance >= this.Price)
+            {
+                CarStatusNotification += car.SuccesWashHandler;
+                car.Balance -= Price;
+            }
+
+            // Not eanaugh money - display the failure notification.
+            // Try to get another car wash. 
+            else
+            {
+                CarStatusNotification += car.LowFoundsHandler;
+            }
+            CarStatusNotification?.Invoke(this);
+            CarStatusNotification = null;
+
+        }
+    }
+}
---
+++ HomeworkDeleagatesEvents/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkDeleagatesEvents
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            List<Car> cars = new List<Car> {
+            new Car(1, "Audi", 500, true),
+            new Car(2, "Toyota", 400, true),
+            new Car(3, "Lada", 300, true),
+            new Car(4, "Mercedes", 350, false),
+            new Car(5, "Pegeout", 600, true),
+            new Car(6, "Citroen", 200, true),
+            new Car(7, "Mazda", 150, true),
+            new Car(8, "Chevrolet", 500, true),
+            new Car(9, "Lotus", 480, true),
+            new Car(10, "Wolksvagen", 700, false)
+            };
+
+
+
+            new CarWash("Bob's wash", 400);
+            new CarWash("Meri's wash", 200);
+
+            //cars[6].GetCleaning();
+
+
+            foreach (Car car in cars)
+            {
+                car.GetCleaning();
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
