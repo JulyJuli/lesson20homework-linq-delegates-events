0d09f027067a48f8b340ef7459fff15bff261ec4
Roman <roma.ne4aev2013@gmail.com>
2020-05-11 22:04:23 +0300

    events

---
+++ HW18/HW18/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace HW18
+{
+    public class Car
+    {
+        private string _model = "Ford";
+        private int _yearOfIssue=1950;
+        public Car(string model, int yearOfIssue, WashingCard washingCard)
+        {
+            Model = model;
+            YearOfIssue = yearOfIssue;
+            WashingCard = washingCard;
+            CarCleanliness = (CarCleanliness)random.Next(0, 2);
+        }
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                if (value != string.Empty)
+                {
+                    _model = value;
+                }
+            }
+        }
+        public int YearOfIssue
+        {
+            get => _yearOfIssue;
+            set
+            {
+                if (value > 1950)
+                {
+                    _yearOfIssue = value;
+                }
+            }
+        }
+        public WashingCard WashingCard { get; set; }
+        public static WashingService WashingService { get; set; }
+        public static Random random = new Random();
+        public CarCleanliness CarCleanliness { get; set; }
+        public override string ToString()
+        {
+            return $"\nWelcome {Model} {YearOfIssue} year of release\n" +
+                $"Car status:{CarCleanliness}";
+        }
+        public static void SuccessfulCarWash(Car car)
+        {
+            Console.WriteLine("Successful wash!");//WashingCard.Balance
+        }
+        public static void InsufficientFundsOnTheCard(Car car)
+        {
+            Console.WriteLine("Not enough funds on the card!");
+        }
+        public static void CarIsClean(Car car)
+        {
+            Console.WriteLine("Your car is clean!");
+        }
+    }
+}
---
+++ HW18/HW18/WashingService.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace HW18
+{
+    public class WashingService
+    {
+        private int _priceOfService;
+        public WashingService(ServiceName serviceName, int priceOfService)
+        {
+            ServiceName = serviceName;
+            PriceOfService = priceOfService;
+        }
+        public ServiceName ServiceName { get; set; }
+        public int PriceOfService
+        {
+            get => _priceOfService;
+            set
+            {
+                if (value > 0)
+                {
+                    _priceOfService = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"{ServiceName} cost {PriceOfService}.";
+        }
+    }
+}
